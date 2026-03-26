d662323edb2ea4209dc4abae3ee667ca3a525067
JOSE DUARTE <bmwcasivuela@hotmail.com>
2026-03-25 20:09:25 -0500

    ejercicios de logica

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log("hell world")
+
+//  let nombre=prompt("dime tu nombre");
+// alert("tu nombre es" + nombre);
+
+// let precio=prompt("pon tu precio final");
+// alert("tu edad es" + edad);
+
+
+
+
+
+// let porcentaje= Number(prompt("de cuanto quieres tu descuento"));
+
+
+// let descuento = (precio * porcentaje)/100;
+// alert ("tu descuento es de" + descuento);
+
+// let preciofina=(precio - descuento);
+// alert("tu precio_final_es" +preciofina);
+
+// console.log("hola tu precio a pagar con el descuento es de " + preciofina);
+
+
+
+let kilometros= Number(prompt("pon un numero en kilometros"));
+let metros= (kilometros * 1000);
+
+alert("el resultado es  " + metros + " metros");
+
